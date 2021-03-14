940cf77b35c83cd83ceb59859114a3ae62a86daf
Maxim <kiber987@gmail.com>
2021-03-14 23:50:43 +0300

    feat: add functionality matrix.length <= 5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+	if ((matrix === undefined) || (matrix.length == 0))
+		return [];
+	//return console.log(matrix.length);
+	let res = [];
+	if (matrix.length == 2)
+		res = [].concat(matrix[0], matrix[1].reverse());
+	else if (matrix.length == 3)
+		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2]);//, matrix[3].reverse());
+	else if (matrix.length == 4)
+		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse());
+	else if (matrix.length == 5)
+		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse(), matrix[4]);
+
+	return (res);	
+	
 }
