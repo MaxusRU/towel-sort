c1bd9e7d84685ab7408847e598c2904c82615200
Maxim <kiber987@gmail.com>
2021-03-24 18:01:48 +0300

    refactor: add alternative solution

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-// You should implement your task here.
+//for matrix.length from 2 to 5
 module.exports = function towelSort (matrix) {
 	if ((matrix === undefined) || (matrix.length == 0))
 		return [];
-	//return console.log(matrix.length);
+	// var result = [].concat(...matrix); // [].concat(matrix[0],matrix[1]... matrix[n] );
 	let res = [];
 	if (matrix.length == 2)
 		res = [].concat(matrix[0], matrix[1].reverse());
 	else if (matrix.length == 3)
-		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2]);//, matrix[3].reverse());
+		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2]);
 	else if (matrix.length == 4)
 		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse());
 	else if (matrix.length == 5)
 		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse(), matrix[4]);
 
 	return (res);	
-	
 }
