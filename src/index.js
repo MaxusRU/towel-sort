6e4ec7c09cbf553a75176dda9abaedf6392b5ae9
Maxim <kiber987@gmail.com>
2021-07-16 17:44:59 +0300

    feat: add solution for any dimension array

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ module.exports = function towelSort (matrix) {
 		return [];
 	// var result = [].concat(...matrix); // [].concat(matrix[0],matrix[1]... matrix[n] );
 	let res = [];
-	if (matrix.length == 2)
+	for (let i=0; i<matrix.length; i++){
+		(i % 2 == 0) ? res = [].concat(res, matrix[i]) : res = [].concat(res, matrix[i].reverse());
+	  }
+/*	if (matrix.length == 2)
 		res = [].concat(matrix[0], matrix[1].reverse());
 	else if (matrix.length == 3)
 		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2]);
@@ -12,6 +15,6 @@ module.exports = function towelSort (matrix) {
 		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse());
 	else if (matrix.length == 5)
 		res = [].concat(matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse(), matrix[4]);
-
+*/
 	return (res);	
 }
